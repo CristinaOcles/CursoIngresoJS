2718d3d371b0f312ee2965854a0f8b3bbe82a43e
CristinaOcles <36174120+CristinaOcles@users.noreply.github.com>
2018-02-06 16:18:44 -0300

    ferreteria facturación
    está por la mitad, continuare...

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,31 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+{
+var precio1;
+var precio2;
+var precio3;
+var resultado;
+var iva;
+
+
+precio1 = document.getElementById('PrecioUno').value;
+precio2 = document.getElementById('PrecioDos').value;
+precio3 = document.getElementById('PrecioTres').value;
+
+
 function Sumar () 
 {
-	
+	resultado = precio1 + precio2 + precio3;
+	alert("La suma de los precios es de: " + resultado);
 }
 function Promedio () 
 {
-	
+	resultado = (precio1 + precio2 + precio3) / 3;
+	alert("el promedio de los precios es: " + resultado);
 }
 function PrecioFinal () 
 {
-	
+		
+}
 }
