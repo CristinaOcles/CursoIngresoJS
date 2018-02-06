b4472c7acdd8d5fa92fb13f3e10fb095329ccb0b
CristinaOcles <36174120+CristinaOcles@users.noreply.github.com>
2018-02-06 16:07:33 -0300

    ejercicios 6, 7,8,9 y 10
    uso de parseInt, operaciones de sumas, restas, multiplicación, división y resto. Porcentajes

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -1,24 +1,69 @@
 /*Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
+ej.: "la Resta es 750"*/
+
+	
 function sumar()
 {	
-	
+	var numero1;
+	var numero2;
+	var suma; 
+
+	numero1 = document.getElementById('numeroUno').value;
+	numero2 = document.getElementById('numeroDos').value;	
+	numero1 = parseInt(1, 10);
+	numero2 = parseInt(2, 10);
+
+	suma = numero1 + numero2;
+	alert("La suma es " + suma);
 }
 
 function restar()
 {
-	
+	var numero1;
+	var numero2;
+	var resta;
+
+	numero1 = parseInt(1, 10);
+	numero2 = parseInt(2, 10);
+	numero1 = document.getElementById('numeroUno').value;
+	numero2 = document.getElementById('numeroDos').value;
+
+	resta = numero1 - numero2;
+	alert("La resta es " + resta);
 }
 
 function multiplicar()
 { 
-	
+	var numero1;
+	var numero2;
+	var multiplicar;
+
+	numero1 = parseInt(1, 10);
+	numero2 = parseInt(2, 10);
+	numero1 = document.getElementById('numeroUno').value;
+	numero2 = document.getElementById('numeroDos').value;
+
+	multiplica = numero1 * numero2;
+	alert("la multiplicacion es " + multiplica);
+
+
 }
 
 function dividir()
 {
-	
+	var numero1;
+	var numero2;
+	var dividir;
+
+	numero1 = parseInt(1, 10);
+	numero2 = parseInt(2, 10);
+	numero1 = document.getElementById('numeroUno').value;
+	numero2 = document.getElementById('numeroDos').value;
+
+	divide = numero1/numero2;
+	alert("La division es " + divide);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+var porcentaje;
+var resultado;
 
+
+sueldo = document.getElementById('sueldo').value;
+sueldo = parseInt(sueldo, 10);
+
+resultado = sueldo*1.1;
+
+document.getElementById('resultado').value = resultado;
 	
 }
