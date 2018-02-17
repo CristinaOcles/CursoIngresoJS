1ccd769284652551db43eead2163301101035876
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-17 10:41:13 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,7 +3,15 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Febrero":
+		alert ("Este mes no tiene m{as de 29 dias");
+		break;
+	default:
+		alert(" Este mes tiene 30 dias o mas");
+		break;
+}
 	
 	
 
