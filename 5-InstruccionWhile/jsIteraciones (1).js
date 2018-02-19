a2e4f385dbb7fb1e17aa37ab10e4741a6870188e
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 19:46:24 -0300

    ejercicio 1 corregido

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 	var contador;
+
+		contador= 0;
+	while(contador<10)
+	{
+		contador++;
+		document.write("<br>" + contador);
+		
+	}
+
+
+}
+/*	
 	contador= 1;
 	while(contador<=10)
 	{
@@ -9,3 +21,22 @@ function Mostrar()
 	}
 
 }//FIN DE LA FUNCIÓN
+
+
+
+/*siempre empezar en 0, regla memotecnica, contador siempre en 0
+contador ariba del document.write,
+definir la variable en 0
+despues cantidad de margen, menos y cantidad de veces a repetir,
+la primer linea dentro del while es la primer linea,
+contador ++ solo cuando sumo de a uno.
+
+codigo correcto
+
+contador= 0;
+while(contador<10)
+{
+	contador++;
+	document.write("<br> + contador");
+	
+}*/
