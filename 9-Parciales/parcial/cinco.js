659e46bb0a00a017f6625182080353dad85e87a5
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 19:09:20 -0300

    ejercicio 5

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
+	var dia;
 
+	dia = prompt ("Ingresa un dia de la semana: ");
+
+	switch(dia)
+	{
+		case "Sabado":
+		case "domingo":
+			alert("es fin de semana");
+			break;
+
+		default:
+			alert("a trabajar!");
+			break;
+	}
 }
