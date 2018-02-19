d6af8c2c0729f03162c724eb559b65ed9442cca2
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 15:12:21 -0300

    parciales 1 2 y 3

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,15 @@
 function Mostrar()
 {
-  
+	  var importe;
+	  var importeFinal;
+
+	  importe = promt("Ingresa importe de producto: ");
+	  importeFinal = (importe * 0.21) + importe;
+
+
+	  document.getElementById('importeFinal').value= importeFinal;
+
+
 }
+
+
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo = document.getElementById('alrgo').value;
+	ancho = document.getElementById('ancho').value;
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = (largo * 2) + (ancho * 2);
+
+	alambre = perimetro * 3;
+
+	alert("se necesitan " + alambre + " metros de alambre. ")
 
 }
