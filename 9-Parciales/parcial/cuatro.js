19882f7c30d149dda9dde4165a4b43de8e00e929
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 15:33:12 -0300

    parcial 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,42 @@
 function Mostrar()
 {
 
+	var numero1;
+	var numero2;
+	var resultado;
+
+	numero1 = promt("ingresa un numero");
+	numero2 = promt("ingresa otro numero");
+	numero1 = parseInt(numero1);
+	numero2 = parseInt(numero2);
+
+
+	if(numero1 == numero2)
+	{
+		resultado = numero1*numero2;
+
+		}else
+		{
+			if(numero1> numero2)
+			{
+				resultado= numero1 - numero2
+
+				}else
+				{
+				
+				resultado = numero1 + numero2
+
+				}
+
+		}//if(numero1> numero2)
+	}    //if(numero1 == numero2)
+
+ document.write("el resultado es: " + resultado);
+
+
 }
+
+
+
+/*  numero1 = promt("ingresa un numero");
+	numero2 = promt("ingresa otro numero");*/
