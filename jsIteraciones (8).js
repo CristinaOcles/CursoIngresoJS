1a3ae335ddedc9c1999a6051edf532ade1112f46
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 22:10:09 -0300

    8 aun sin terminar

---
+++ jsIteraciones (8).js
@@ -0,0 +1,37 @@
+function Mostrar()
+{
+
+	var contador=0;
+	var positivo=0;
+	var negativo=1;
+	
+	var respuesta=true;
+
+while(respuesta == true)
+	{
+		
+		numero = prompt ("ingrese numero");
+
+		contador++;
+
+		numero = parseInt(numero);
+
+		if(numero<0)
+		{
+			negativo= negativo + numero;
+		}
+		if(numero>0)
+		{
+			positivo= positivo+numero;
+		}
+
+		respuesta= confirm("Quiere ingresar otro numero?");
+		
+
+	}
+
+
+document.getElementById('suma').value=positivo;
+document.getElementById('producto').value=negativo;
+
+}//FIN DE LA FUNCIÓN
