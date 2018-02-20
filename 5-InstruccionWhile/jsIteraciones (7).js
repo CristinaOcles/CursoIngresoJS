3703304f7e77d55c73e4d3be28b0de28984fccd5
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-19 21:53:36 -0300

    ejercicio 7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -6,6 +6,23 @@ function Mostrar()
 	var respuesta='si';
 
 
+	while(respuesta == "si")
+	{
+		
+		numero = prompt ("ingrese numero");
+
+		contador++;
+
+		numero = parseInt(numero);
+
+		acumulador= acumulador+numero;
+
+		respuesta= prompt("Quiere ingresar otro numero?");
+		
+
+	}
+
+
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
 
