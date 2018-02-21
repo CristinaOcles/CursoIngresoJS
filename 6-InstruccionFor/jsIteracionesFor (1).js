0b7e49e5dbc14cbc5c283c1b8bc9cfc887707f7f
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-20 21:13:50 -0300

    ejercicios 1 y 2 con el profesor

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,90 @@
-function Mostrar()
-{
 
+function Mostrar()
+{	
+	var contadorDePositivos=0;
+	var contadorDeNegativos=0;
+	var contadorDeCeros=0;
+	var contadorDePar=0;
+	var contadorDeImpar=0;
+	var contador50=0;
+
+
+	for(contador=-100;contador<100;contador++)
+	{
+		if(contador>0)
+		{
+			contadorDePositivos++;
+		}else
+		{
+			if(contador<0)
+			{
+
+				contadorDeNegativos++;
+			}else
+			{
+				contadorDeCeros++;
+			}
+
+		}
+		if(contador%2==0 && contador!=0)
+		{
+			contadorDePar++;
+		}else
+			{
+				if(contador!=0)
+				{
+			
+				contadorDeImpar++;
+				}
+			}
+		
+
+		switch (contador)
+		{
+			case 50:
+				contador50++;
+				break;
+		}
+
+
+
+	}//for(contador=-100;contador<100;contador++)
+	
+	document.write("<br> positivos" + contadorDePositivos);
+	document.write("<br>negativos" + contadorDeNegativos);
+	document.write("<br>cero" + contadorDeCeros); 
+	document.write("<br>par" + contadorDePar); 
+	document.write("<br>impar" + contadorDeImpar); 
+	document.write("<br>50" + contador50); 
 }
+
+
+
+
+
+
+
+
+/*
+
+    	var contador=0;
+	for(contador=0; contador<10; )
+	{
+		contador++
+		document.write("<br>" + contador);
+
+		if(contador==10)
+		{
+			document.write("break"+contador);
+			break;
+
+		}
+		if(contador==5)
+		{
+			continue;
+		}
+		document.write("<br>" + "continue" + contador);
+
+	}
+
+*/
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,111 @@
 function Mostrar()
 {
 
+	var numeroRandom;
+	var contador1=0;
+	var contador2=0;
+	var contador3=0;
+	var contador4=0;
+	var contador5=0;
+	var contador6=0;
+	var contador7=0;
+	var contador8=0;
+	var contador9=0;
+	var contador10=0;
+
+
+
+	for(contador=0;contador<100;contador++;)
+	{
+		numeroRandom=Math.floor(Math.random()*10);
+		document.write("<br>" numeroRandom);
+		switch(numeroRandom)
+		{
+			case 0:
+				contador1++;
+				break;
+			case 1:
+				contador2++;
+				break;
+			case 2:
+				contador3++;
+				break;
+			case 3:
+				contador4++;
+				break;		
+			case 4:
+				contador5++;
+				break;
+			case 5:
+				contador6++;
+				break;
+			case 6:
+				contador7++;
+				break;
+			case 7:
+				contador8++;
+				break;
+			case 8:
+				contador9++;
+				break;		
+			case 9:
+				contador10++;
+				break;
+
+		}switch(numeroRandom)
+
+	}for(contador=0;contador<100;contador++;)
+
+	porcen0= (contador0/ (contador+1)) * 100
+	porcen0= Math.round(porcen0);
+	document.write("<br>cantidad 0: " + contador0 +porcen0+ "% :");
+	
+	porcen1= (contador0/ (contador+1)) * 100
+	porcen1= Math.round(porcen1);
+	document.write("<br>cantidad 0: " + contador0 +porcen1+ "% :");
+
+	porcen2= (contador0/ (contador+1)) * 100
+	porcen2= Math.roundporcen2);
+	document.write("<br>cantidad 0: " + contador0 porcen2+ "% :");
+
+	porcen3= (contador0/ (contador+1)) * 100
+	porcen3= Math.round(porcen3);
+	document.write("<br>cantidad 0: " + contador0 +porcen3+ "% :");
+
+	porcen4= (contador0/ (contador+1)) * 100
+	porcen4= Math.round(porcen4);
+	document.write("<br>cantidad 0: " + contador0 +porcen4+ "% :");
+
+	porcen5= (contador0/ (contador+1)) * 100
+	porcen5= Math.round(porcen5);
+	document.write("<br>cantidad 0: " + contador0 +porcen5+ "% :");
+	
+	porcen6= (contador0/ (contador+1)) * 100
+	porcen6= Math.round(porcen6);
+	document.write("<br>cantidad 0: " + contador0 +porcen6+ "% :");
+
+	porcen7= (contador0/ (contador+1)) * 100
+	porcen7= Math.round(porcen7);
+	document.write("<br>cantidad 0: " + contador0 +porcen7+ "% :");
+
+	porcen8= (contador0/ (contador+1)) * 100
+	porcen8= Math.round(porcen8);
+	document.write("<br>cantidad 0: " + contador0 +porcen8+ "% :");
+
+	porcen9= (contador0/ (contador+1)) * 100
+	porcen9= Math.round(porcen9);
+	document.write("<br>cantidad 0: " + contador0 +porcen9+ "% :");
+
+	porcen10= (contador0/ (contador+1)) * 100
+	porcen10= Math.round(porcen10);
+	document.write("<br>cantidad 0: " + contador0 +porcen10+ "% :");
+
+
+
+
+	
+
+	
+
 
 }
