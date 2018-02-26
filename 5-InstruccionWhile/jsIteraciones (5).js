9a4ff58e095c2aada24b60215111ab21163eb304
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-26 13:25:07 -0300

    ejercicio 5

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,22 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+	var sexo = prompt("ingrese f ó m .");
+
+
+		while (sexo != "f" && sexo != "m")
+		{
+			
+			alert ("Sexo incorrecto");
+			sexo = prompt("ingrese f ó m .");
+
+		}
+
+		document.getElementById('Sexo').value=sexo;
 
 
 
-document.getElementById('Sexo').value=sexo;
+
+
 
 }//FIN DE LA FUNCIÓN
