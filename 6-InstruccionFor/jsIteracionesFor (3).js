5539460141b26fcfc68a587e13e95f790f24b2f5
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-02-26 20:17:56 -0300

    ejercicio 3

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,16 @@
 function Mostrar()
 {
+	var contador=0;
+	var repeticiones = prompt("ingrese el número de repeticiones");
+	
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	repeticiones = parseInt(repeticiones);
 
 
+	for(contador=0;contador<repeticiones; contador++)
+		{
+			alert("Hola UTN FRA");
+			
+		}
 
 }//FIN DE LA FUNCIÓN
