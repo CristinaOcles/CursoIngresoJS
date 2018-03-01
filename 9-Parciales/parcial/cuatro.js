582a6cebc4322cd4cb3fd836dad4f21bacce73c4
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-03-01 13:20:27 -0300

    ejercicio 4 corregido

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,8 +1,8 @@
 function Mostrar()
 {
 
-	var numero1;
-	var numero2;
+	var numero1=1;
+	var numero2=1;
 	var resultado;
 
 	numero1 = prompt("ingresa un numero");
@@ -11,28 +11,23 @@ function Mostrar()
 	numero2 = parseInt(numero2);
 
 
-	if(numero1 == numero2)
+	if (numero1 == numero2)
 	{
-		resultado = numero1*numero2;
+		resultado = numero1 * numero2;
+		
 
+	}else
+	{
+		if(numero1>numero2)
+		{
+			resultado = numero1-numero2;
 		}else
 		{
-			if(numero1> numero2)
-			{
-				resultado= numero1 - numero2;
+			resultado = numero1+numero2;
+		}
+	}
 
-				}else
-				{
-				
-				resultado = numero1 + numero2;
-
-				}
-
-		}//if(numero1> numero2)
-	}    //if(numero1 == numero2)
-
- document.write("el resultado es: " + resultado);
-
+	document.write("el resultado es: " + resultado);
 
 }
 
