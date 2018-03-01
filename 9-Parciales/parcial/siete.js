2b0a15e5a1c748665860863df44981ad8439b870
Cristina <36174120+CristinaOcles@users.noreply.github.com>
2018-03-01 15:32:23 -0300

    ejercicio 7 sin terminar

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,50 @@
 function Mostrar()
 {
+	var nombre;
+	var notas;
+	var sexo;
+	var promedio;
+	var notaBaja;
+	var varones;
+	var contador;
+
+
+	for(contador=0; contador<99; contador++);
+	{
+		notas = prompt ("ingrese la nota del alumno");
+		notas=parseInt(notas);
+
+		while (notas<0 || notas>10)
+		{
+			alert("Nota ingresada incorrecta");
+			notas = prompt ("ingrese la nota del alumno");
+			notas=parseInt(notas);
+
+		}
+
+		sexo= prompt("Ingrese el sexo del alumno (f/m)");
+
+		while(sexo!="f" || sexo!="m" || !isNan)
+		{
+			sexo= prompt("Sexo incorrecto, vuelva a ingresarlo(f/m)");	
+		}
+
+
+		promedio = promedio + notas;
+
+
+
+
+
+
+
+
+	}//for(contador=0; contador<99; contador++);
+
+
+
+
+
+
 
 }
